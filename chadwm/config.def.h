e3e7ec6ca4b63ba55e837841a7feb0353ee9db6c
hayley <hayley@blanca>
2022-05-17 16:36:29 -0500

    change default border to 1 pixel

--- chadwm/config.def.h
+++ chadwm/config.def.h
@@ -3,8 +3,10 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int default_border = 0;   /* to switch back to default border after dynamic border resizing via keybinds */
+// static const unsigned int borderpx  = 0;        /* border pixel of windows */
+// static const unsigned int default_border = 0;   /* to switch back to default border after dynamic border resizing via keybinds */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int default_border = 1;   /* to switch back to default border after dynamic border resizing via keybinds */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
